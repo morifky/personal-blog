e89f05a5b4beb616b99aa6161051013aadb1e41f
morifky <rifky.satriaperdana@gmail.com>
2025-06-09 20:36:19 +0700

    update project history

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,16 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Hicard App',
+    description: `Built the backend for Hicard, a retail consumer membership app, using Nest.js and PostgreSQL. Shipped on June 2023`,
+    imgSrc: '/static/images/hicard.png',
+    href: 'https://play.google.com/store/apps/details?id=id.co.hypermart.hicard&hl=id&pli=1',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Chateauxde App',
+    description: `Built a fullstack e-commerce site for coffee using React.js, Nest.js, and PostgreSQL — launched in August 2022.`,
+    imgSrc: '/static/images/chateauxde.png',
+    href: 'https://chateauxde.com/prelaunch',
   },
 ]
 
